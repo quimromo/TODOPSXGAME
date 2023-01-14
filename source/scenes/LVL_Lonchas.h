30f7b9876af9b64f6d5941b655d17fb5d07ec55a
Quod Soler <quodsoler@gmail.com>
2023-01-14 14:54:32 +0100

    Optimized tiles

--- source/scenes/LVL_Lonchas.h
+++ source/scenes/LVL_Lonchas.h
@@ -4,25 +4,25 @@
 
 #include "obstacle01.h"
 
-extern unsigned long _binary_assets_textures_waterTilesetSmall_tim_start[];
+extern unsigned long _binary_assets_textures_waterTilesetFinal_tim_start[];
 
 tdActor levelData_LVL_Lonchas_actors[] = {
-{.position = {0,0,0}, .meshData={.mesh=&lonchaBase_Mesh, .texture_tim=_binary_assets_textures_waterTilesetSmall_tim_start},
+{.position = {0,0,0}, .meshData={.mesh=&lonchaBase_Mesh, .texture_tim=_binary_assets_textures_waterTilesetFinal_tim_start},
 .scale = {4000,4000,4000},
 .rotation={0, 0, 0}},
-{.position = {-800,0,-680}, .meshData={.mesh=&obstacle01_Mesh, .texture_tim=_binary_assets_textures_waterTilesetSmall_tim_start},
+{.position = {-800,0,-680}, .meshData={.mesh=&obstacle01_Mesh, .texture_tim=_binary_assets_textures_waterTilesetFinal_tim_start},
 .scale = {4000,4000,4000},
 .rotation={0, 0, 0}},
-{.position = {1810,0,2900}, .meshData={.mesh=&obstacle01_Mesh, .texture_tim=_binary_assets_textures_waterTilesetSmall_tim_start},
+{.position = {1810,0,2900}, .meshData={.mesh=&obstacle01_Mesh, .texture_tim=_binary_assets_textures_waterTilesetFinal_tim_start},
 .scale = {4000,4000,4000},
 .rotation={0, 0, 0}},
-{.position = {2090,0,-3720}, .meshData={.mesh=&obstacle01_Mesh, .texture_tim=_binary_assets_textures_waterTilesetSmall_tim_start},
+{.position = {2090,0,-3720}, .meshData={.mesh=&obstacle01_Mesh, .texture_tim=_binary_assets_textures_waterTilesetFinal_tim_start},
 .scale = {4000,4000,4000},
 .rotation={0, 0, 0}},
-{.position = {-2030,0,2900}, .meshData={.mesh=&obstacle01_Mesh, .texture_tim=_binary_assets_textures_waterTilesetSmall_tim_start},
+{.position = {-2030,0,2900}, .meshData={.mesh=&obstacle01_Mesh, .texture_tim=_binary_assets_textures_waterTilesetFinal_tim_start},
 .scale = {4000,4000,4000},
 .rotation={0, 0, 0}},
-{.position = {-1810,0,-3820}, .meshData={.mesh=&obstacle01_Mesh, .texture_tim=_binary_assets_textures_waterTilesetSmall_tim_start},
+{.position = {-1810,0,-3820}, .meshData={.mesh=&obstacle01_Mesh, .texture_tim=_binary_assets_textures_waterTilesetFinal_tim_start},
 .scale = {4000,4000,4000},
 .rotation={0, 444, 0}}
 };
